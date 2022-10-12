7b3117883af6243d6842c2fc2e56cc9169d6bd23
Angus Tomlinson <angus.b.tomlinson@gmail.com>
2022-10-12 00:07:50 -0700

    Add neovim

--- install.sh
+++ install.sh
@@ -16,6 +16,11 @@ sudo apt install -y vim
 
 stow --restow --target=$HOME vim
 
+if ! $(grep 'alias v="vim"' ~/.bashrc)
+then
+    echo 'alias v="vim"' >> ~/.bashrc
+fi
+
 # Enable editing shell commands with vi
 if ! $(grep "set -o vi" ~/.bashrc)
 then
@@ -29,7 +34,25 @@ sudo apt install -y tmux
 stow --restow --target=$HOME tmux
 
 # If TPM is not installed, download it and install plugins
-if [ ! -d "$HOME/.tmux/plugins/tpm/" ]
+if ! -d "$HOME/.tmux/plugins/tpm/"
 then
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm && ~/.tmux/plugins/tpm/bin/install_plugins
 fi
+
+# Install neovim
+echo "Install neovim..."
+
+local_nvim_pkg_path = $HOME/Downloads/nvim-linux64.db
+
+wget -O local_nvim_pkg_path https://github.com/neovim/neovim/releases/download/v0.8.0/nvim-linux64.deb
+
+sudo dpkg -i local_nvim_pkg_path && rm local_nvim_pkg_path
+
+if ! $(grep 'alias nv="XDG_DATA_HOME=~/.local/share XDG_CONFIG_HOME=~/.config nvim"' ~/.bashrc)
+then
+    echo 'alias nv="XDG_DATA_HOME=~/.local/share XDG_CONFIG_HOME=~/.config nvim"' >> ~/.bashrc
+fi
+
+# Reload .bashrc
+echo "Reload .bashrc..."
+source $HOME/.bashrc
